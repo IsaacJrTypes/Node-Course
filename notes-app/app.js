48d4e679a3c96e74065f20224fe0db8d87859e35
Isaac J <duckhunterjr@gmail.com>
2021-08-25 14:55:49 -0700

    adds lesson 3

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,6 @@
-//Call fs module
-const fs = require("fs");
-//call fs to write txt
-//fs.writeFileSync("notes.txt", "This file was created by Node.js!");
+//use require with '.' means current level path (this directory), '/' means root level, then specify file (within root level) to execute
+require("./utils.js");
 
-//challenge: Append a message t notes.txt
-//1. use appendFileSync to append to the file
-fs.appendFileSync('notes.txt','I am appending more text using appendFileSync()')
-//2.Run the script
-//done
-//3.CHeck your work by opening the file and viewing the appended txt
-//It worked!! Next time add space in front of message
+const name = "Isaac";
+
+console.log(name);
