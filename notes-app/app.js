1dcfeccdcbc40a48296c481ddf22735ee6d35f9c
Isaac J <duckhunterjr@gmail.com>
2021-08-25 14:41:36 -0700

    adds lesson 2

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,12 @@
 //Call fs module
-const fs = require('fs')
-//
-fs.writeFileSync('notes.txt','This file was created by Node.js!')
+const fs = require("fs");
+//call fs to write txt
+//fs.writeFileSync("notes.txt", "This file was created by Node.js!");
+
+//challenge: Append a message t notes.txt
+//1. use appendFileSync to append to the file
+fs.appendFileSync('notes.txt','I am appending more text using appendFileSync()')
+//2.Run the script
+//done
+//3.CHeck your work by opening the file and viewing the appended txt
+//It worked!! Next time add space in front of message
