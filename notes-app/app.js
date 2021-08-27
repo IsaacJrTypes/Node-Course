7c2747a6f4516f7dadb2377c97b5d3c8da863003
Isaac J <duckhunterjr@gmail.com>
2021-08-27 15:17:08 -0700

    add new notes file and lesson

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,6 +1,20 @@
+//Challenge: define and use a function in a new file:
+//
+//1. create new file called notes.js
+//2. create getNotes function that returns "Your notes.."
+//3. Export getNotes function
+//4. from app.js, load in and call the function print message to console.
+const getNotes = require("./notes.js");
+
+//Note call function before exe file, otherwise function is passed but not called
+const msg = getNotes();
+
+console.log(msg);
+
 //use require with '.' means current level path (this directory), '/' means root level, then specify file (within root level) to execute
-require("./utils.js");
+// const add = require("./utils.js");
+//Export var name can be different than name1 on this page
 
-const name = "Isaac";
+// const sum = add(4, -2);
 
-console.log(name);
+// console.log(sum);
--- notes-app/utils.js
+++ notes-app/utils.js
@@ -1,1 +1,12 @@
-console.log('utils.js')
+console.log("utils.js");
+
+const name1 = "Isaac";
+
+const add = function (a, b) {
+  return a + b;
+};
+
+//export variable for global directory scope
+module.exports = add;
+
+//Use type "clear" in terminal to clear
