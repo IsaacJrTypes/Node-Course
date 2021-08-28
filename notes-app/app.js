0a190982a9c51c7b9b15c39f34547c85bef01bd1
Isaac J <duckhunterjr@gmail.com>
2021-08-28 11:25:48 -0700

    add validator package

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,6 +4,9 @@
 //2. create getNotes function that returns "Your notes.."
 //3. Export getNotes function
 //4. from app.js, load in and call the function print message to console.
+
+const validator = require("validator");
+
 const getNotes = require("./notes.js");
 
 //Note call function before exe file, otherwise function is passed but not called
@@ -11,6 +14,11 @@ const msg = getNotes();
 
 console.log(msg);
 
+//Example using validator, console will print true
+console.log(validator.isEmail("duck@gmail.com"));
+
+/* Additional Notes */
+//
 //use require with '.' means current level path (this directory), '/' means root level, then specify file (within root level) to execute
 // const add = require("./utils.js");
 //Export var name can be different than name1 on this page
