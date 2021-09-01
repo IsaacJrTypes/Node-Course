b4f54a2ade20922fcf65b4f83d42014b2f2dcc63
Isaac J <duckhunterjr@gmail.com>
2021-09-01 13:24:07 -0700

    add overwritting fs commands

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,11 +1,19 @@
-const fs = require('fs')
-const book = {
-  title: "Dopamine Nation",
-  author: "Anna Lembke",
-};
+const fs = require("fs");
+const { userInfo } = require("os");
+// const book = {
+//   title: "Dopamine Nation",
+//   author: "Anna Lembke",
+// };
 
-const bookJSON = JSON.stringify(book); //stringify object into JSON format
-console.log(bookJSON);
+// const bookJSON = JSON.stringify(book); //stringify object into JSON format
+// fs.writeFileSync("1-json.json", bookJSON);//creates new file and stores JSON entry
 
-const parsedData = JSON.parse(bookJSON);//Converts JSON to an object
-console.log(parsedData.author);
+const dataBuffer = fs.readFileSync("1-json.json"); //stores binary ver of file
+const dataJSON = dataBuffer.toString(); //stringify binary data into js string
+const user = JSON.parse(dataJSON); //parses string into an object
+
+user.name = "Isaac";
+user.age = 50;
+
+const userJSON = JSON.stringify(user);
+fs.writeFileSync("1-json.json", userJSON);
