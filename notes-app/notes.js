ea76ac7a3028fdcba9814fcad55089e1342f27eb
Isaac J <duckhunterjr@gmail.com>
2021-09-07 15:42:29 -0700

    add remove function with chalk attirbutes

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,4 +1,6 @@
 const fs = require("fs");
+const chalk = require("chalk");
+const { callbackify } = require("util");
 
 const getNotes = function () {
   return "Your notes..";
@@ -7,7 +9,8 @@ const getNotes = function () {
 const addNote = function (title, body) {
   const notes = loadNotes(); //loads notes
   const duplicateNotes = notes.filter(function (note) {
-    return note.title === title;
+    //finds duplicate of notes
+    return note.title === title; //returns true if titles are duplicates
   });
 
   if (duplicateNotes.length === 0) {
@@ -17,14 +20,24 @@ const addNote = function (title, body) {
       body: body,
     });
     savedNotes(notes); //saves notes
-    console.log("New Note added!");
+    console.log(chalk.inverse.white("New Note added!"));
   } else {
-    console.log("Note title taken!");
+    console.log(chalk.inverse.red("Note title taken!"));
   }
 };
 
 const removeNote = function (title) {
-  console.log(title);
+  const notes = loadNotes();
+  const notesToKeep = notes.filter(function (note) {
+    return note.title !== title; //Returns true if titles are not a match
+  });
+
+  if (notes.length > notesToKeep.length) {
+    console.log(chalk.green.inverse("Note removed!!"));
+    savedNotes(notesToKeep); //calls saved notes and passes notes to keep
+  } else {
+    console.log(chalk.red.inverse("No note found!"));
+  }
 };
 
 const savedNotes = function (notes) {
